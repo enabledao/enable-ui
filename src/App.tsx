a2eea41f87ce365d09da8cbc0cd7b39b60eb3626
iMaur <urmauur@gmail.com>
2019-06-16 01:29:06 +0700

    setup font
    - using font Source Sans Pro from google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ const App: React.FC = () => {
   return (
     <Fragment>
       <NormalizeStyle />
+      <h1>Enable</h1>
+      <h1>The quick brown brown fox jump over the lazy</h1>
+      <h2>The quick brown brown fox jump over the lazy</h2>
+      <h3>The quick brown brown fox jump over the lazy</h3>
+      <h4>The quick brown brown fox jump over the lazy</h4>
+      <h5>The quick brown brown fox jump over the lazy</h5>
+      <h6>The quick brown brown fox jump over the lazy</h6>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quidem
         excepturi hic suscipit, illo dolorum est, fugiat nostrum assumenda
