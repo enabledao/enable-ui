56c17085d1225a4b49f200bc2e63f10828b99aaf
iMaur <urmauur@gmail.com>
2019-07-11 18:02:31 +0700

    add static bottm button withdrawal

--- src/components/pages/myLoan/withdrawals/index.tsx
+++ src/components/pages/myLoan/withdrawals/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "../../../../styles/bases";
 import { Margin } from "../../../../styles/utils";
 import { Row, Col, Button } from "../../../lib";
-import { WithdrawalBox } from "./styled";
+import { WithdrawalBox, WithdrawalActionMobile } from "./styled";
 
 const Withdrawal: React.FC = () => (
   <Container>
@@ -18,11 +18,14 @@ const Withdrawal: React.FC = () => (
               </Col>
             </Row>
             <Row justify="center" text="center">
-              <Col lg={4} md={6} sm={12}>
+              <Col lg={4} md={6} sm="hidden">
                 <Button color="purple">Withdraw</Button>
               </Col>
             </Row>
           </WithdrawalBox>
+          <WithdrawalActionMobile>
+            <Button color="purple">Withdraw</Button>
+          </WithdrawalActionMobile>
         </Margin>
       </Col>
     </Row>
