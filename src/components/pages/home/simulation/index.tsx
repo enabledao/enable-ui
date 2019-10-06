50a863dfe0792d8ff1906567bbe48b76607fad73
dennisjuanito <dennisjuanito@gmail.com>
2019-10-05 19:59:46 -0700

    Move Sponsorships

--- src/components/pages/home/simulation/index.tsx
+++ src/components/pages/home/simulation/index.tsx
@@ -22,6 +22,7 @@ import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import ReactTooltip from 'react-tooltip'
 import ListContributor from '../listContributor'
 import BorrowerIdentityCard from '../borrowerIdentityCard'
+import CompanySponsorships from './companySponsorships'
 
 interface SimuLationReturnProps extends RouteComponentProps<any> {
     contributors?: any
@@ -253,75 +254,7 @@ class SimuLationReturn extends React.Component<
                     {/* </Col>
                     </Row> */}
                 </SimulationWrapper>
-
-                <Margin vertical={40}>
-                    <h5>Company Sponsorships</h5>
-                </Margin>
-                <Margin vertical={24}>
-                    <SimulationWrapper>
-                        <h5>Gold Tier</h5>
-                        <h6>Lend 30,000 Dai or more</h6>
-                        <p>
-                            I will be your Campus Ambassador at Cornell, and
-                            promote your company, help with event preparations
-                            and publicity
-                        </p>
-                        <Margin top={40} bottom={30}>
-                            <h6 style={{ color: '#21b549', cursor: 'pointer' }}>
-                                Expected Repayment Calculator
-                            </h6>
-                            <h5>
-                                46,612 Dai&nbsp;
-                                <small>
-                                    &nbsp;(simulated $86,320 annual salary)
-                                </small>
-                            </h5>
-                        </Margin>
-                        <Button
-                            color="green"
-                            outline
-                            onClick={() =>
-                                window.open(
-                                    'https://calendly.com/inesfund/video-interview'
-                                )
-                            }
-                        >
-                            Schedule Video Interview
-                        </Button>
-                    </SimulationWrapper>
-                </Margin>
-                <Margin vertical={24}>
-                    <SimulationWrapper>
-                        <h5>Silver Tier</h5>
-                        <h6>Lend 5,000 Dai or more</h6>
-                        <p>
-                            I will put your company's stickers on my laptop, as
-                            well as my graduation robe in 2 years time
-                        </p>
-                        <Margin top={40} bottom={30}>
-                            <h6 style={{ color: '#21b549', cursor: 'pointer' }}>
-                                Expected Repayment Calculator
-                            </h6>
-                            <h5>
-                                7,768 Dai&nbsp;
-                                <small>
-                                    &nbsp;(simulated $86,320 annual salary)
-                                </small>
-                            </h5>
-                        </Margin>
-                        <Button
-                            color="green"
-                            outline
-                            onClick={() =>
-                                window.open(
-                                    'https://calendly.com/inesfund/video-interview'
-                                )
-                            }
-                        >
-                            Schedule Video Interview
-                        </Button>
-                    </SimulationWrapper>
-                </Margin>
+                <CompanySponsorships />
                 <ListContributor
                     contributors={contributors}
                     paymentToken={paymentToken}
